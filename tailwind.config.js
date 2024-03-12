d6443d15fc6edbc99f5af7c868f978e96d47fb65
Nikhil Jugdan <thenikhiljugdan@gmail.com>
2024-03-12 08:33:50 +0530

    Add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.svelte', './src/**/*.css'],
+	darkMode: false,
+	theme: {
+		extend: {}
+	},
+	plugins: []
+};
+
